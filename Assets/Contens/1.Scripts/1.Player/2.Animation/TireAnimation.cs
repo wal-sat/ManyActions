eb7498fc33d54f1884e02d4fab010be8ec7a8f41
wal <wal.12.wh@gmail.com>
2024-10-18 20:31:04 +0900

    ルールタイルとタイヤのアニメーション

---
+++ Assets/Contens/1.Scripts/1.Player/2.Animation/TireAnimation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TireAnimation : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] Sprite[] sprites;
+    [SerializeField] private float ANIMATION_TIME;
+
+    private float _timer;
+    private int _index;
+
+    private void FixedUpdate()
+    {
+        // いづれどこかからこれを呼び出す
+        Debug.Log("a");
+        AnimationUpdate();
+    }
+
+    public void AnimationUpdate()
+    {
+        _timer += Time.deltaTime;
+
+        if (_timer >= ANIMATION_TIME)
+        {
+            _timer = 0;
+            _index ++;
+
+            if (_index >= sprites.Length) _index = 0;
+
+            spriteRenderer.sprite = sprites[_index];
+        }
+    }
+}
