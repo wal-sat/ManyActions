a1600ab2c17538985508d4dfa8b4445466bc70d8
wal <133085395+wal-sat@users.noreply.github.com>
2024-12-10 20:50:35 +0900

    背景の動きもいい感じに直した

--- Assets/Contens/1.Scripts/5.GameScene/uGUI/BackgroundBar.cs
+++ Assets/Contens/1.Scripts/5.GameScene/uGUI/BackgroundBar.cs
@@ -6,17 +6,30 @@ using DG.Tweening;
 
 public class BackgroundBar : MonoBehaviour
 {
-    [SerializeField] Transform StartPosition;
-    [SerializeField] Transform EndPosition;
+    [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] Transform LeftTransform;
+    [SerializeField] Transform RightTransform;
     [SerializeField] float SPEED;
+
+    private float _LeftRightDistance;
+
+    private void Awake()
+    {
+        _LeftRightDistance = RightTransform.localPosition.x - LeftTransform.localPosition.x;
+    }
 
     private void FixedUpdate()
     {
-        this.gameObject.transform.localPosition += new Vector3(SPEED * Time.deltaTime, 0f ,0f); 
+        if (playerMovement.isFacingRight) this.gameObject.transform.localPosition += new Vector3(-SPEED * Time.deltaTime, 0f ,0f); 
+        else this.gameObject.transform.localPosition += new Vector3(SPEED * Time.deltaTime, 0f ,0f); 
 
-        if (this.gameObject.transform.localPosition.x > EndPosition.localPosition.x)
+        if (this.gameObject.transform.localPosition.x < LeftTransform.localPosition.x)
         {
-            this.gameObject.transform.localPosition = StartPosition.localPosition;
+            this.gameObject.transform.localPosition += new Vector3(_LeftRightDistance, 0f, 0f);
+        }
+        if (RightTransform.localPosition.x < this.gameObject.transform.localPosition.x)
+        {
+            this.gameObject.transform.localPosition += new Vector3(-_LeftRightDistance, 0f, 0f);
         }   
     }
 }
