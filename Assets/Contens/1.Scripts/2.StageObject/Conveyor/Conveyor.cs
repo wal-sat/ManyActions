404ff35f734bffc73e120b2997d8a3e96ec4976f
wal <wal.12.wh@gmail.com>
2024-12-31 00:12:34 +0900

    コンベア修正

--- Assets/Contens/1.Scripts/2.StageObject/Conveyor/Conveyor.cs
+++ Assets/Contens/1.Scripts/2.StageObject/Conveyor/Conveyor.cs
@@ -10,13 +10,32 @@ public class Conveyor : MonoBehaviour
     [SerializeField] bool IS_RIGHT_DIRECTION;
 
     private const float OFFSET = 0.2f;
+    private const float EXIT_BUFFER = 0.1f;
+
+    private float _timer;
+    private bool _isTimer;
 
     private void Start()
     {
         playerMovement._conveyorPlusSpeed = PLUS_SPEED;
     }
+    private void FixedUpdate()
+    {
+        if (_isTimer)
+        {
+            _timer += Time.deltaTime;
 
-    private void OnCollisionEnter2D(Collision2D other)
+            if (_timer > EXIT_BUFFER)
+            {
+                _timer = 0;
+                _isTimer = false;
+
+                playerMovement.isLandingConveyor = false;
+            }
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
@@ -26,6 +45,9 @@ public class Conveyor : MonoBehaviour
                 if (!IS_RIGHT_DIRECTION) playerMovement.isLandingConveyor_left = true;
 
                 playerMovement.isLandingConveyor = true;
+
+                _timer = 0;
+                _isTimer = false;
             }
         }
     }
@@ -33,10 +55,7 @@ public class Conveyor : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            if (this.transform.position.y + this.gameObject.transform.localScale.y / 2 - OFFSET <= PlayerLandingChecker.position.y) 
-            {
-                playerMovement.isLandingConveyor = false;
-            }
+            _isTimer = true;
         }
     }
 }
