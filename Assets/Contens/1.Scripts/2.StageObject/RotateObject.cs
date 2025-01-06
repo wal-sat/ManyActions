9e141fca459a0163a60a8e4d7252a6bf5d94aa62
wal <133085395+wal-sat@users.noreply.github.com>
2025-01-06 22:03:57 +0900

    回転オブジェクトでけた

--- Assets/Contens/1.Scripts/2.StageObject/RotateObject.cs
+++ Assets/Contens/1.Scripts/2.StageObject/RotateObject.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WarpPointRotation : MonoBehaviour
+public class RotateObject : MonoBehaviour
 {
     [SerializeField] float ROTATE_SPEED;
 
     private void FixedUpdate()
     {
-        this.gameObject.transform.Rotate(0, 0, ROTATE_SPEED * Time.deltaTime);
+        this.gameObject.transform.Rotate(0, 0, ROTATE_SPEED * Time.deltaTime, Space.World);
     }
 }
