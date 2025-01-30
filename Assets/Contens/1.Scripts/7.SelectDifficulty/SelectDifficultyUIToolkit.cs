1b74d1f08fe6d2f772523c705b95c191b9906ee9
wal <133085395+wal-sat@users.noreply.github.com>
2025-01-31 00:51:50 +0900

    背景と難易度セレクト画面作った

--- Assets/Contens/1.Scripts/7.SelectDifficulty/SelectDifficultyUIToolkit.cs
+++ Assets/Contens/1.Scripts/7.SelectDifficulty/SelectDifficultyUIToolkit.cs
@@ -9,6 +9,10 @@ public class SelectDifficultyUIToolkit : MonoBehaviour
     [SerializeField] private GameObject UIToolkit;
 
     private VisualElement[][] _cards = new VisualElement[2][];
+    private VisualElement _normalStageIconSleep;
+    private VisualElement _normalStageIconSelected;
+    private VisualElement _extraStageIconSleep;
+    private VisualElement _extraStageIconSelected;
     private VisualElement[] _confirmOptions = new VisualElement[2];
     private VisualElement _confirmPanel;
     private Label _questiontext;
@@ -26,6 +30,11 @@ public class SelectDifficultyUIToolkit : MonoBehaviour
         _cards[1][0] = root.Q<VisualElement>("Card_1");
         _cards[0][1] = root.Q<VisualElement>("Card_2");
         _cards[1][1] = root.Q<VisualElement>("Card_2");
+
+        _normalStageIconSleep = root.Q<VisualElement>("NormalStageIcon_sleep");
+        _normalStageIconSelected = root.Q<VisualElement>("NormalStageIcon_selected");
+        _extraStageIconSleep = root.Q<VisualElement>("ExtraStageIcon_sleep");
+        _extraStageIconSelected = root.Q<VisualElement>("ExtraStageIcon_selected");
 
         _confirmOptions[0] = root.Q<VisualElement>("ConfirmOptions0");
         _confirmOptions[1] = root.Q<VisualElement>("ConfirmOptions1");
@@ -49,11 +58,25 @@ public class SelectDifficultyUIToolkit : MonoBehaviour
         _cards[1][0].RemoveFromClassList("Card--Selected");
         _cards[0][1].RemoveFromClassList("Card-mini--Selected");
         _cards[1][1].RemoveFromClassList("Card-mini--Selected");
-
         if (0 <= x && x < _cards.Length)
         {
             if (y == 0) _cards[x][y].AddToClassList("Card--Selected");
             else if (y == 1) _cards[x][y].AddToClassList("Card-mini--Selected");
+        }
+
+        _normalStageIconSleep.RemoveFromClassList("StageIcon--off");
+        _normalStageIconSelected.AddToClassList("StageIcon--off");
+        _extraStageIconSleep.RemoveFromClassList("StageIcon--off");
+        _extraStageIconSelected.AddToClassList("StageIcon--off");
+        if (x == 0 && y == 0) 
+        {
+            _normalStageIconSleep.AddToClassList("StageIcon--off");
+            _normalStageIconSelected.RemoveFromClassList("StageIcon--off");
+        }
+        else if (x == 1 && y == 0)
+        {
+            _extraStageIconSleep.AddToClassList("StageIcon--off");
+            _extraStageIconSelected.RemoveFromClassList("StageIcon--off");
         }
     }
     public void ConfirmOptionsSelect(int index)
---
+++ Assets/Contens/1.Scripts/9.Utility/Background/BackgroundBar.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class BackgroundBar : MonoBehaviour
+{
+    [SerializeField] PlayerMovement playerMovement;
+    [SerializeField] Transform LeftTransform;
+    [SerializeField] Transform RightTransform;
+    [SerializeField] float SPEED;
+
+    private float _LeftRightDistance;
+
+    private void Awake()
+    {
+        _LeftRightDistance = RightTransform.position.x - LeftTransform.position.x;
+    }
+
+    private void FixedUpdate()
+    {
+        if (playerMovement == null)
+        {
+            this.gameObject.transform.position += new Vector3(-SPEED * Time.deltaTime, 0f ,0f); 
+        }
+        else
+        {
+            if (playerMovement.isFacingRight) this.gameObject.transform.position += new Vector3(-SPEED * Time.deltaTime, 0f ,0f); 
+            else this.gameObject.transform.position += new Vector3(SPEED * Time.deltaTime, 0f ,0f); 
+
+        }
+
+        if (this.gameObject.transform.position.x < LeftTransform.position.x)
+        {
+            this.gameObject.transform.position += new Vector3(_LeftRightDistance, 0f, 0f);
+        }
+        if (RightTransform.position.x < this.gameObject.transform.position.x)
+        {
+            this.gameObject.transform.position += new Vector3(-_LeftRightDistance, 0f, 0f);
+        }   
+    }
+}
