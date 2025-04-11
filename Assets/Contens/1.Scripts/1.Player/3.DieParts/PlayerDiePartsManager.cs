f5e733b35d93c359c723f927534c98a9e58f46aa
wal <wal.12.wh@gmail.com>
2025-04-11 09:57:25 +0900

    背景作り途中

--- Assets/Contens/1.Scripts/1.Player/3.DieParts/PlayerDiePartsManager.cs
+++ Assets/Contens/1.Scripts/1.Player/3.DieParts/PlayerDiePartsManager.cs
@@ -9,14 +9,33 @@ public class PlayerDiePartsManager : MonoBehaviour
     [SerializeField] float SPEED;
     [SerializeField] Vector2 DESTROY_DISTANCE;
 
+
     private List<GameObject> _parts = new List<GameObject>();
+    private int[] layerInts = new int[3];
+    private int _layerIndex;
+    int layerIndex
+    {
+        get => _layerIndex;
+        set
+        {
+            _layerIndex = value;
+            if (_layerIndex >= 3) _layerIndex = 0;
+        }
+    }
+
+    private void Awake()
+    {
+        for (int i = 0; i < 3; i++) layerInts[i] = LayerMask.NameToLayer("PlayerDieParts" + i);
+        layerIndex = 0;
+    }
 
     public void Die(Vector3 playerPosition)
     {
         foreach (var part in dieParts)
         {
-            GameObject newPart = Instantiate(part, new Vector3(playerPosition.x, playerPosition.y, -4f), Quaternion.identity);
+            GameObject newPart = Instantiate(part, new Vector3(playerPosition.x, playerPosition.y, -4.5f + 0.5f * layerIndex), Quaternion.identity);
             newPart.SetActive(true);
+            newPart.layer = layerInts[layerIndex++];
             newPart.GetComponent<DieParts>().Initialize( (int) DESTROY_DISTANCE.x, (int) DESTROY_DISTANCE.y, OnDestory);
             Rigidbody2D rb = newPart.GetComponent<Rigidbody2D>();
 
