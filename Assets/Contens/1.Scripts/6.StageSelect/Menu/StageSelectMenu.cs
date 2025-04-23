1732219f0055e2dfbf4ea3d0d19e450601e41bcd
wal <133085395+wal-sat@users.noreply.github.com>
2025-04-24 01:16:57 +0900

    スプライトとかとか

--- Assets/Contens/1.Scripts/6.StageSelect/Menu/StageSelectMenu.cs
+++ Assets/Contens/1.Scripts/6.StageSelect/Menu/StageSelectMenu.cs
@@ -10,6 +10,8 @@ public class StageSelectMenu : MonoBehaviour
     [SerializeField] public StageSelectMenuConfirm stageSelectMenuConfirm;
     [SerializeField] public StageSelectMenuUIToolkit stageSelectMenuUIToolkit;
     [SerializeField] StageSelectCameraMovement stageSelectCameraMovement;
+    [SerializeField] Sprite acquiredActionBlind;
+    [SerializeField] Sprite stageImageBlind;
 
     public Action<StageSelectSceneStatus> ChangeStatus;
 
@@ -167,9 +169,14 @@ public class StageSelectMenu : MonoBehaviour
 
     private void DisplayStageInfomation(StageData stageData)
     {
-        stageSelectMenuUIToolkit.AcquireActionImageChange(stageIndex, undergroundIndex, reverseIndex, stageData.acquireActionImage);
         stageSelectMenuUIToolkit.StageNameLabelChange(stageIndex, undergroundIndex, reverseIndex, stageData.worldName, stageData.stageName);
-        stageSelectMenuUIToolkit.StageImageChange(stageIndex, undergroundIndex, reverseIndex, stageData.stageImage);
+        if (stageData.isReleased) {
+            stageSelectMenuUIToolkit.AcquireActionImageChange(stageIndex, undergroundIndex, reverseIndex, stageData.acquireActionImage);
+            stageSelectMenuUIToolkit.StageImageChange(stageIndex, undergroundIndex, reverseIndex, stageData.stageImage);
+        } else {
+            stageSelectMenuUIToolkit.AcquireActionImageChange(stageIndex, undergroundIndex, reverseIndex, acquiredActionBlind);
+            stageSelectMenuUIToolkit.StageImageChange(stageIndex, undergroundIndex, reverseIndex, stageImageBlind);
+        }
         for (int i = 0; i < 5; i++)
         {
             stageSelectMenuUIToolkit.GearIconAcquired(i, stageIndex, undergroundIndex, reverseIndex, stageData.gearAcquire[i]);
